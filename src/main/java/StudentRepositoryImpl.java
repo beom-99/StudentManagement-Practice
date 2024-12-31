b76a98bdf95bb5a42c454304b8aa04129bac72eb
kang <qjarkd705766@gmail.com>
2024-12-31 10:32:26 +0900

    repositoty 완성

--- src/main/java/StudentRepositoryImpl.java
+++ src/main/java/StudentRepositoryImpl.java
@@ -1,28 +1,66 @@
+import java.util.ArrayList;
 import java.util.List;
 
+// 인터페이스를 구현한 클래스는 구현체 클
 public class StudentRepositoryImpl implements StudentRepository {
-    @Override
+    
+	private final List<Student> students = new ArrayList<>();
+	private int nextId = 1;
+	
+	public StudentRepositoryImpl() {
+		System.out.println("StudentRepository 생성");
+	}
+	
+	@Override
     public List<Student> findAll() {
-        return null;
+        return new ArrayList<>(students);
     }
 
     @Override
     public Student findById(int id) {
-        return null;
+//    	for(int i =0;i<students.size();i++) {
+//    		if(students.get(i).getId() == id) {
+//    			return students.get(i);
+//    		}
+//    	}
+//    	for each 문
+    	for (Student student : students) {
+    		if(student.getId()==id) {
+    			return student;
+    		}
+    	}
+    	
+    	return null;
     }
-
+      
+    
+     
+    
+    
     @Override
     public void save(Student student) {
-
+    	student = new Student(nextId++, student.getName(),student.getMajor());
+    	students.add(student);
     }
 
     @Override
     public void update(int id, Student updatedStudent) {
-
+    	Student student = findById(id);
+    	if(student != null) {
+    		student.setName(updatedStudent.getName());
+    		student.setMajor(updatedStudent.getMajor());
+    	}
     }
 
     @Override
     public void delete(int id) {
-
+//    	  students.removeIf(student -> student.getId() == id); 
+    	
+    	for (int i=0; i< students.size();i++) {
+    		if(students.get(i).getId()==id) {
+    			students.remove(i);
+    			break;
+    		}
+    	}
     }
 }
